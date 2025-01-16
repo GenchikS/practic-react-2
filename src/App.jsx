182aa156714ca9414660931d86f2c599cafd5933
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-01-16 21:19:46 +0200

    v.1.2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Product from './pr1/component/Poroduct/Product.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {/* pract1 */}
+      <h1>Practic 1</h1>
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default App
