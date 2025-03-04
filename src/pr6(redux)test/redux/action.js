059685a5b1a1b5668f37809c78d6737cec0e9c8d
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-03-04 23:10:34 +0200

    corr

--- src/pr6(redux)test/redux/action.js
+++ src/pr6(redux)test/redux/action.js
@@ -4,3 +4,7 @@ import { createAction } from "@reduxjs/toolkit";
 export const citySelect = createAction("task/citySelect")
 export const transmissionSelect = createAction("task/transmission");
 export const engineSelect = createAction("task/engine");
+
+//  можна перевірити
+//  console.log(citySelect("hello"));
+// { type: "task/citySelect", payload: "hello" }
