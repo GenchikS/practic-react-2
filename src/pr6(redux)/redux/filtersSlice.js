1017b6be122c9d97e9465d3650b94c687efa94a9
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-03-10 12:30:05 +0200

    reduxSlice

--- src/pr6(redux)/redux/filtersSlice.js
+++ src/pr6(redux)/redux/filtersSlice.js
@@ -1,22 +1,43 @@
-import { createAction } from "@reduxjs/toolkit";
+// import { createAction } from "@reduxjs/toolkit";
 
-export const statusFilter = createAction("filters/statusFilter");
+import { createSlice } from "@reduxjs/toolkit";
+
+// export const statusFilter = createAction("filters/statusFilter");
 
 
-const initialState = {
-    filters: {
-        status: ["all"]
-    }
-}
+// const initialState = {
+//     filters: {
+//         status: ["all"]
+//     }
+// }
 
-export default function filtersReducer (state = initialState.filters, action){
-  switch (action.type) {
-    case "filters/statusFilter":
+// export default function filtersReducer (state = initialState.filters, action){
+//   switch (action.type) {
+//     case "filters/statusFilter":
+//       return {
+//         ...state,
+//           status: action.payload,
+//        };
+//     default:
+//       return state;
+//   }
+// };
+
+
+const slice = createSlice({
+  name: "filters",
+  initialState: {
+    status: ["all"],
+  },
+  reducers: {
+    statusFilter: (state, action) => {
       return {
         ...state,
-          status: action.payload,
-       };
-    default:
-      return state;
-  }
-};
+        status: action.payload,
+      };
+    }
+  },
+});
+
+export const { statusFilter } = slice.actions;
+export default slice.reducer;
